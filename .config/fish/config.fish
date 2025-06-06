a08b028119a694c94c107d281327a78d09fc1592
DustyMcRusty <159064652+DustyMcRusty@users.noreply.github.com>
2025-06-06 12:15:01 -0300

    first commit

---
+++ .config/fish/config.fish
@@ -0,0 +1,30 @@
+if status is-interactive
+  # wal -Renq
+
+    # Commands to run in interactive sessions can go here
+end
+
+function l
+	ls -la
+end
+
+function cls
+	clear
+end
+
+function qq
+	exit
+end
+
+function szyp
+	sudo zypper $argv
+end
+
+function zyp
+	zypper $argv
+end
+
+# Created by `pipx` on 2025-05-27 14:23:02
+set PATH $PATH /home/apolonio/.local/bin
+
+zoxide --cmd cd init fish | source
