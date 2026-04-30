240d1cfed957211621c7a13b996a4fce8b15f06d
DustyMcRusty (Erick F. Brochier) <159064652+DustyMcRusty@users.noreply.github.com>
2026-04-30 15:52:36 -0300

    grug

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -35,7 +35,6 @@
 		{ src = 'https://github.com/L3MON4D3/LuaSnip' },
 		{ src = 'https://github.com/saghen/blink.cmp' },
 		{ src = 'https://github.com/saghen/blink.lib' },
-		{ src = 'https://github.com/letorbi/vim-colors-modern-borland', name = 'borland-colors' },
 		{ src = 'https://github.com/m4xshen/autoclose.nvim', name = 'autoclose' },
 		{ src = 'https://github.com/mason-org/mason.nvim' },
 		{ src = 'https://github.com/neovim/nvim-lspconfig' },
